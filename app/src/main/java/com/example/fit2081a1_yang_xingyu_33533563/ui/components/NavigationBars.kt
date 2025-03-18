2fd690500f4178c194c0f9185aa89b483a7d8c62
Eric <xyan0147@student.monash.edu>
2025-03-18 12:24:02 +0800

    fix render errors on top bar

--- app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/ui/components/NavigationBars.kt
+++ app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/ui/components/NavigationBars.kt
@@ -15,10 +15,6 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
