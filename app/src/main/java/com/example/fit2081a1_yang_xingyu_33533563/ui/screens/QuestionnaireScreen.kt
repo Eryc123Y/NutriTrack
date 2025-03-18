aa3c0ebc5501b53429f585d42475373627ac7af2
Eric <xyan0147@student.monash.edu>
2025-03-18 16:56:30 +0800

    feat: Implement QuestionnaireScreen with TopNavigationBar and layout

--- app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/ui/screens/QuestionnaireScreen.kt
+++ app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/ui/screens/QuestionnaireScreen.kt
@@ -1,2 +1,40 @@
 package com.example.fit2081a1_yang_xingyu_33533563.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.fit2081a1_yang_xingyu_33533563.ui.components.TopNavigationBar
+
+@Preview(showSystemUi = true)
+@Composable
+fun QuestionnaireScreen(
+    onNavigate: (String) -> Unit = {},
+    onBackClick: () -> Unit = {}
+) {
+    Scaffold (
+        topBar = {
+            TopNavigationBar(
+                title = "Food Intake Questionnaire",
+                showBackButton = true,
+                onBackButtonClick = onBackClick
+            )
+        },
+    ){ innerPadding ->
+        Column(
+            modifier = Modifier.fillMaxSize().padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("Questionnaire", style = TextStyle(fontSize = 24.sp))
+        }
+    }
+}
