f8cf2026d937e2c3af1bf5aa7cff5fb6cf7ad654
Eric <xyan0147@student.monash.edu>
2025-03-18 18:09:33 +0800

    feat: Add Color import to ValidationUtils for enhanced input validation

--- app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/util/ValidationUtils.kt
+++ app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/util/ValidationUtils.kt
@@ -1,7 +1,11 @@
 package com.example.fit2081a1_yang_xingyu_33533563.util
 
+
+
 /**
  * Created by Xingyu Yang
  * SharedPreferencesManager takes care of input validation for the app.
  * including login, registration, and questionnaire responses.
  */
+
+
