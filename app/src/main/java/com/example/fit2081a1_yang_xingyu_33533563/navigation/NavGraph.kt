d0988642db6e05a47b8e3d8cecef1a3469607abc
Eric <xyan0147@student.monash.edu>
2025-03-18 18:24:59 +0800

    feat: Add navigation transitions and integrate QuestionnaireScreen into NavGraph

--- app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/navigation/NavGraph.kt
+++ app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/navigation/NavGraph.kt
@@ -1,5 +1,10 @@
 package com.example.fit2081a1_yang_xingyu_33533563.navigation
 
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -8,6 +13,7 @@ import com.example.fit2081a1_yang_xingyu_33533563.ui.screens.HomeScreen
 import com.example.fit2081a1_yang_xingyu_33533563.ui.screens.InsightsScreen
 import com.example.fit2081a1_yang_xingyu_33533563.ui.screens.LoginScreen
 import com.example.fit2081a1_yang_xingyu_33533563.ui.screens.NutriCoachScreen
+import com.example.fit2081a1_yang_xingyu_33533563.ui.screens.QuestionnaireScreen
 import com.example.fit2081a1_yang_xingyu_33533563.ui.screens.SettingsScreen
 import com.example.fit2081a1_yang_xingyu_33533563.ui.screens.WelcomeScreen
 
@@ -16,13 +22,38 @@ import com.example.fit2081a1_yang_xingyu_33533563.ui.screens.WelcomeScreen
  * This module defines navigation routes
  */
 
+
+/**
+ * Composable function for navigation of the app
+ * @return Unit
+ * Router:
+ * WelcomeScreen -> LoginScreen -> MainScreens
+ * MainScreens: HomeScreen, InsightsScreen, NutriCoachScreen, SettingsScreen
+ * navigator names:
+ * WelcomeScreen : welcome
+ * LoginScreen : login
+ * HomeScreen : home
+ * InsightsScreen : insights
+ * NutriCoachScreen : nutricoach
+ * SettingsScreen : settings
+ */
 @Composable
 fun AppNavigation() {
     val navController = rememberNavController()
 
     NavHost(
         navController = navController,
-        startDestination = "welcome"
+        startDestination = "welcome",
+        // default transition
+        enterTransition = { EnterTransition.None },
+        exitTransition = { ExitTransition.None },
+        popEnterTransition = { slideInHorizontally(initialOffsetX = { -1000 }, animationSpec = tween(300)) },
+        popExitTransition = { slideOutHorizontally(targetOffsetX = { 1000 }, animationSpec = tween(300)) }
+
+        // previously used transitions, only put it here for future reference
+//        enterTransition = { slideInHorizontally(initialOffsetX = { 1000 }, animationSpec = tween(300)) },
+//        exitTransition = { slideOutHorizontally(targetOffsetX = { -1000 }, animationSpec = tween(300)) },
+
     ) {
         //Welcome
         composable("welcome") {
@@ -39,13 +70,20 @@ fun AppNavigation() {
         //HomeScreen
         composable("home") {
             HomeScreen(
-                onNavigate = { route -> navController.navigate(route) },
-                onBackClick = { navController.popBackStack() }
+                onNavigate = { route -> navController.navigate(route) }
             )
         }
         //InsightScreen
         composable("insights") {
             InsightsScreen(
+                onNavigate = { route -> navController.navigate(route) },
+                onBackClick = { navController.popBackStack() }
+            )
+        }
+
+        //QuestionnaireScreen
+        composable("questionnaire") {
+            QuestionnaireScreen(
                 onNavigate = { route -> navController.navigate(route) },
                 onBackClick = { navController.popBackStack() }
             )
