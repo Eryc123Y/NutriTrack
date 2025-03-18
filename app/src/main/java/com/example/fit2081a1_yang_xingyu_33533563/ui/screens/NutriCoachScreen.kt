591a0f032bb5d3ce610c465b75877abc0533eb0f
Eric <xyan0147@student.monash.edu>
2025-03-18 12:24:55 +0800

    add scaffold for testing only. Not implemented in A1

--- app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/ui/screens/NutriCoachScreen.kt
+++ app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/ui/screens/NutriCoachScreen.kt
@@ -3,20 +3,45 @@ package com.example.fit2081a1_yang_xingyu_33533563.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import com.example.fit2081a1_yang_xingyu_33533563.ui.components.BottomNavigationBar
+import com.example.fit2081a1_yang_xingyu_33533563.ui.components.TopNavigationBar
 
+@Preview(showSystemUi = true)
 @Composable
-fun NutriCoachScreen() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text("NutriCoach", style = TextStyle(fontSize = 24.sp))
+fun NutriCoachScreen(
+    onNavigate: (String) -> Unit = {},
+    onBackClick: () -> Unit = {}
+) {
+    Scaffold (
+        topBar = {
+            TopNavigationBar(
+                title = "NutriCoach",
+                showBackButton = true,
+                onBackButtonClick = onBackClick
+            )
+        },
+        bottomBar = {
+            BottomNavigationBar(
+                currentRoute = "nutriCoach",
+                onNavigate = onNavigate
+            )
+        }
+    ){ innerPadding ->
+        Column(
+            modifier = Modifier.fillMaxSize().padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("NutriCoach", style = TextStyle(fontSize = 24.sp))
+        }
     }
 }
--- app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/ui/screens/SettingsScreen.kt
@@ -3,20 +3,57 @@ package com.example.fit2081a1_yang_xingyu_33533563.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import com.example.fit2081a1_yang_xingyu_33533563.ui.components.BottomNavigationBar
+import com.example.fit2081a1_yang_xingyu_33533563.ui.components.TopNavigationBar
 
+/**
+ * Created by Eric
+ * This module contains setting layout
+ */
+
+
+/**
+ * Composable function for the Settings screen
+ * @param onNavigate callback function for navigating to other screens
+ * @param onBackClick callback function for navigating back to the previous screen
+ * @return Unit
+ */
+@Preview(showSystemUi = true)
 @Composable
-fun SettingsScreen() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text("Settings", style = TextStyle(fontSize = 24.sp))
+fun SettingsScreen(
+    onNavigate: (String) -> Unit = {},
+    onBackClick: () -> Unit = {}
+) {
+    Scaffold (
+        topBar = {
+            TopNavigationBar(
+                title = "Settings",
+                showBackButton = true,
+                onBackButtonClick = onBackClick
+            )
+        },
+        bottomBar = {
+            BottomNavigationBar(
+                currentRoute = "settings",
+                onNavigate = onNavigate
+            )
+        }
+    ){ innerPadding ->
+        Column(
+            modifier = Modifier.fillMaxSize().padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("Settings", style = TextStyle(fontSize = 24.sp))
+        }
     }
 }
