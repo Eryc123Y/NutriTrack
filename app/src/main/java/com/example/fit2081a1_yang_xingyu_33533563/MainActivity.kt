8b390fe7d952e9f0051b722efb550398265ec0f8
Eric <xyan0147@student.monash.edu>
2025-03-18 12:26:17 +0800

    refactor: Simplify MainActivity by removing unnecessary Scaffold and padding

--- app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/MainActivity.kt
+++ app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/MainActivity.kt
@@ -4,20 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.example.fit2081a1_yang_xingyu_33533563.navigation.AppNavigation
-import com.example.fit2081a1_yang_xingyu_33533563.ui.screens.HomeScreen
-import com.example.fit2081a1_yang_xingyu_33533563.ui.screens.LoginScreen
-import com.example.fit2081a1_yang_xingyu_33533563.ui.screens.WelcomeScreen
 import com.example.fit2081a1_yang_xingyu_33533563.ui.theme.FIT2081A1Theme
 
 class MainActivity : ComponentActivity() {
@@ -26,9 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FIT2081A1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    AppNavigation(modifier = Modifier.padding(innerPadding))
-                }
+                AppNavigation()
             }
         }
     }
