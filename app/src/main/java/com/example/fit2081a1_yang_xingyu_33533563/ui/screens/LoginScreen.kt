0b7e2bac28b16e7367916b416c7787fd1f72730e
Eric <xyan0147@student.monash.edu>
2025-03-18 16:57:21 +0800

    feat: Add horizontal divider to LoginScreen for improved layout

--- app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/ui/screens/LoginScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.OutlinedTextField
@@ -150,6 +151,8 @@ fun LoginScreen(onNavigateToHome: () -> Unit = {}) {
             ) {
                 Text("Continue")
             }
+
+            HorizontalDivider(modifier = Modifier.padding(8.dp), thickness = 2.dp)
 
         }
     }
