ee58f8654f90d3585d35838036d471bb8170a02b
Eric <xyan0147@student.monash.edu>
2025-03-23 01:00:59 +0800

    refactor: Rename property in Gender enum for clarity

--- app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/data/model/Gender.kt
+++ app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/data/model/Gender.kt
@@ -2,7 +2,7 @@ package com.example.fit2081a1_yang_xingyu_33533563.data.model
 
 import java.util.Locale
 
-enum class Gender(val infoName: String) {
+enum class Gender(val genderName: String) {
     MALE("male"),
     FEMALE("female");
 
