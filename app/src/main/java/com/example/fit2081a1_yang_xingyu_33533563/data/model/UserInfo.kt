74bcec43734eeaa2133fa05ce3ffec4c5c270c48
Eric <xyan0147@student.monash.edu>
2025-03-23 00:11:46 +0800

    feat: Add GENDER constant to UserInfo enum for user gender representation

--- app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/data/model/UserInfo.kt
+++ app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/data/model/UserInfo.kt
@@ -2,7 +2,8 @@ package com.example.fit2081a1_yang_xingyu_33533563.data.model
 
 enum class UserInfo(val infoName: String) {
     USERID("User_ID"),
-    PHONENUMBER("PhoneNumber");
+    PHONENUMBER("PhoneNumber"),
+    GENDER("userGender");
 
 
 }
