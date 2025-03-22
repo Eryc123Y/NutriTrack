6ae5d34dfafe606fba4863e3170316f564e425c6
Eric <xyan0147@student.monash.edu>
2025-03-22 22:00:54 +0800

    feat: Add getLoginValidationData function to retrieve user login data from CSV

--- app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/util/ValidationUtils.kt
+++ app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/util/ValidationUtils.kt
@@ -1,5 +1,8 @@
 package com.example.fit2081a1_yang_xingyu_33533563.util
 
+import android.content.Context
+import com.example.fit2081a1_yang_xingyu_33533563.data.csv.readColumn
+import com.example.fit2081a1_yang_xingyu_33533563.data.model.UserInfo
 
 
 /**
@@ -8,4 +11,17 @@ package com.example.fit2081a1_yang_xingyu_33533563.util
  * including login, registration, and questionnaire responses.
  */
 
-
+/**
+ * Get a map of UserId to a given attribute
+ */
+fun getLoginValidationData(
+    context: Context,
+    targetColumn: String = UserInfo.PHONENUMBER.infoName,
+    filePath: String = "testUsers.csv"
+): Map<String, Any> {
+    var userData = mapOf<String, Any>()
+    val idCol = readColumn(context, UserInfo.USERID.infoName,  filePath)
+    val targetCol = readColumn(context, targetColumn, filePath)
+    userData = idCol.zip(targetCol).toMap() as Map<String, Any>
+    return userData
+}
