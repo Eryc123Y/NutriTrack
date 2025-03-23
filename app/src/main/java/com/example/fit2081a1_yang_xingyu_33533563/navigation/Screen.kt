471288c8c76a9c72c8cbe6f3fa2256d104fcd20a
Eric <xyan0147@student.monash.edu>
2025-03-24 00:08:42 +0800

    feat: Update Screen enum documentation for improved clarity on navigation usage

--- app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/navigation/Screen.kt
+++ app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/navigation/Screen.kt
@@ -5,6 +5,12 @@ package com.example.fit2081a1_yang_xingyu_33533563.navigation
  * This file contains enum class Screen that map a screen component to a router name string
  */
 
+/**
+ * Enum class Screen、
+ * Screen is used to map a screen component to a router name string
+ * Used for navigation in the app
+ * @param route: String
+ */
 enum class Screen(val route: String) {
     Login("login"),
     Home("home"),
