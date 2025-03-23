bb8cca6f13eabd057c3dacd414d253116e0f1ac0
Eric <xyan0147@student.monash.edu>
2025-03-24 00:09:27 +0800

    feat: Add initial TimePickerComponent and TimePickerRow composables

--- app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/ui/components/TimePickerComponent.kt
+++ app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/ui/components/TimePickerComponent.kt
@@ -1,2 +1,16 @@
 package com.example.fit2081a1_yang_xingyu_33533563.ui.components
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+// todo: implement TimePickerComponent
+fun TimePickerComponent() {
+
+}
+
+@Composable
+fun TimePickerRow(text: String) {
+
+}
