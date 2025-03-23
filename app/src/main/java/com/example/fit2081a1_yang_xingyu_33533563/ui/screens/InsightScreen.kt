b072f1b254516a06fc9c8054296e4bdbd9687dcf
Eric <xyan0147@student.monash.edu>
2025-03-24 02:39:43 +0800

    feat: Refactor InsightScreen to enhance layout and integrate ScoreInterface with user ID retrieval

--- app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/ui/screens/InsightScreen.kt
+++ app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/ui/screens/InsightScreen.kt
@@ -4,17 +4,22 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.fit2081a1_yang_xingyu_33533563.navigation.Screen
 import com.example.fit2081a1_yang_xingyu_33533563.ui.components.BottomNavigationBar
+import com.example.fit2081a1_yang_xingyu_33533563.ui.components.ScoreInterface
 import com.example.fit2081a1_yang_xingyu_33533563.ui.components.TopNavigationBar
+import com.example.fit2081a1_yang_xingyu_33533563.util.SharedPreferencesManager
 
 @Preview(showBackground = true)
 @Composable
@@ -37,24 +42,30 @@ fun InsightsScreen(
             )
         }
     ){ innerPadding ->
-        Column(
+        Surface(
             modifier = Modifier.fillMaxSize().padding(innerPadding),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            color = MaterialTheme.colorScheme.background
         ) {
-            Text("Insights", style = TextStyle(fontSize = 24.sp))
-            scoreProgressBars()
-            shareAndImproveDiet()
+            val context = LocalContext.current
+            val prefManager = SharedPreferencesManager(context)
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                val userID = prefManager.getCurrentUser()
+                ScoreInterface(userID.toString())
+            }
         }
     }
 }
 
 @Composable
-fun scoreProgressBars() {
+fun ScoreProgressBars() {
 
 }
 
 @Composable
-fun shareAndImproveDiet() {
+fun ShareAndImproveDiet() {
 
 }
