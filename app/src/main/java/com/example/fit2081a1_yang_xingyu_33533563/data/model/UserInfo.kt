4b377e3bf301ddd8089ca15915232bf3b1becef0
Eric <xyan0147@student.monash.edu>
2025-03-24 02:39:18 +0800

    feat: Update GENDER enum value for consistency in user information

--- app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/data/model/UserInfo.kt
+++ app/src/main/java/com/example/fit2081a1_yang_xingyu_33533563/data/model/UserInfo.kt
@@ -3,7 +3,7 @@ package com.example.fit2081a1_yang_xingyu_33533563.data.model
 enum class UserInfo(val infoName: String) {
     USERID("User_ID"),
     PHONENUMBER("PhoneNumber"),
-    GENDER("userGender");
+    GENDER("Sex");
 
 
 }
